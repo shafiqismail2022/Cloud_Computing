7844880152d556dbb421e07e8fc3ed9b2f947dd7
shafiqismail2022 <shafiqismail664@gmail.com>
2025-04-16 14:58:02 +0300

    Clear API Code

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def predict():
 if __name__ == '__main__':
     # Run on localhost for testing
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)Fir
+    app.run(host='0.0.0.0', port=5000)
